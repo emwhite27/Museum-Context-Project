c7947dd53e6329490c9669c3811de26b5b1263f2
VEMSOK <vems.ok@ubc.ca>
2024-02-09 14:14:24 -0800

    StarterWorldDemo

--- Assets/Scripts/CanvasTextManager.cs
+++ Assets/Scripts/CanvasTextManager.cs
@@ -5,10 +5,22 @@ public class CanvasTextManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI title;
     [SerializeField] private TextMeshProUGUI content;
-    
+
+    private Camera main;
+
+    private void Awake()
+    {
+        main = Camera.main;
+    }
+
     public void UpdateText(string titleText, string contentText)
     {
         title.text = titleText;
         content.text = contentText;
+    }
+
+    private void Update()
+    {
+        transform.forward = main.transform.forward;
     }
 }
